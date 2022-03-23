a7edf0cb4ddd841345e40436cceb1a62e6e35f28
Bishal Mukherjee <bishal.mukherjee@shoshinschool.com>
2022-03-23 19:57:51 +0530

    delete .eslintrc, rename prodeslintrc script added2

--- build/client_build.js
+++ build/client_build.js
@@ -32,34 +32,37 @@ function buildReactClient() {
     }
 
     console.log(`[Client v2] Cloning branch ${clientBranch}.`);
-
-    exec(
-      `git clone https://${process.env.CLIENT_GITHUB_TOKEN}@github.com/shoshinschool/Shoshin-Client-App-New.git --single-branch --branch ${clientBranch} tmp`
-    );
-    exec("mkdir -p react_client");
-
-    exec("touch .env.development .env.test .env.production", "tmp");
-
-    const ENV_FILE_CONTENT =
-      "REACT_APP_APPLICATION_BASE_URL=$APPLICATION_BASE_URL";
-
-    exec('echo "' + ENV_FILE_CONTENT + '" > .env.development', "tmp");
-    exec('echo "' + ENV_FILE_CONTENT + '" > .env.test', "tmp");
-    exec('echo "' + ENV_FILE_CONTENT + '" > .env.production', "tmp");
-
-    exec("npm install", "tmp");
-    console.log("_______________npm install completed_________________");
     exec("ls");
+    exec("cd tmp");
+    exec("ls");
+
+    // exec(
+    //   `git clone https://${process.env.CLIENT_GITHUB_TOKEN}@github.com/shoshinschool/Shoshin-Client-App-New.git --single-branch --branch ${clientBranch} tmp`
+    // );
+    // exec("mkdir -p react_client");
+
+    // exec("touch .env.development .env.test .env.production", "tmp");
+
+    // const ENV_FILE_CONTENT =
+    //   "REACT_APP_APPLICATION_BASE_URL=$APPLICATION_BASE_URL";
+
+    // exec('echo "' + ENV_FILE_CONTENT + '" > .env.development', "tmp");
+    // exec('echo "' + ENV_FILE_CONTENT + '" > .env.test', "tmp");
+    // exec('echo "' + ENV_FILE_CONTENT + '" > .env.production', "tmp");
+
+    // exec("npm install", "tmp");
+    // console.log("_______________npm install completed_________________");
+    // exec("ls");
     // exec("rm .eslintrc");
     // console.log(".eslintrc file deleted");
     // exec("ls");
     // exec("mv prodeslintrc.txt .eslintrc");
     // console.log("prodeslintrc.txt ----> .eslintrc");
     // exec("ls");
-    exec("npm run build", "tmp");
-    // exec("npm run build-upload", "tmp");
+    // exec("npm run build", "tmp");
+    // // exec("npm run build-upload", "tmp");
 
-    moveFiles();
+    // moveFiles();
 
     exec("rm -rf tmp");
     console.log("[Client v2] Client build completed");
